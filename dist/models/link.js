70fa30ff43e5ba8a7a1e07960c24b0ac15a641d8
Senorsen <senorsen.zhang@gmail.com>
2016-03-30 21:58:28 +0800

    Finished the basis

---
+++ dist/models/link.js
@@ -0,0 +1,24 @@
+'use strict';
+var Sequelize = require('sequelize');
+
+module.exports = function (sequelize, DataTypes) {
+    var Link = sequelize.define('Link', { 
+        linkId: { 
+            type: Sequelize.UUID, 
+            defaultValue: Sequelize.UUIDV4, 
+            primaryKey: true }, 
+
+        linkToken: { 
+            type: Sequelize.STRING, 
+            unique: true }, 
+
+        linkUrl: { 
+            type: Sequelize.STRING(4096) } }, 
+
+    { 
+        classMethods: {}, 
+        charset: 'utf8', 
+        collate: 'utf8_general_ci' });
+
+
+    return Link;};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,51 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
+let models = require('../models');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/:linkToken', async function (req, res, next) {
+    try {
+        let linkObject = await models.Link.findOne({
+            where: {
+                linkToken: req.params.linkToken
+            }
+        });
+        if (!linkObject) {
+            return next();
+        }
+        res.header('X-Redir', linkObject.linkUrl);
+        res.redirect(linkObject.linkUrl);
+    } catch (e) {
+        console.error(e);
+        res.status(500);
+        res.render('error', {
+            message: e.message || '出错啦',
+            error: e
+        });
+    }
+});
+
+router.get('/create', async function (req, res, next) {
+    try {
+        if (!req.query.linkToken || !req.query.linkUrl) {
+            return res.json({
+                code: 1,
+                msg: '参数错误'
+            });
+        }
+        let linkObject = await models.Link.create({
+            linkToken: req.query.linkToken,
+            linkUrl: req.query.linkUrl
+        });
+        res.json(linkObject);
+    } catch (e) {
+        console.error(e);
+        res.status(500);
+        res.render('error', {
+            message: e.message || '出错啦',
+            error: e
+        });
+    }
 });
 
 module.exports = router;
